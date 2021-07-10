764c3c3b5f042c039132f383a888a33a98a187f7
Automation <github-actions@github.com>
2021-07-10 06:49:00 +0000

    chore: self mutation

--- .projenrc.js
+++ .projenrc.js
@@ -3,11 +3,11 @@ const { AwsCdkConstructLibrary, DependenciesUpgradeMechanism } = require('projen
 const AUTOMATION_TOKEN = 'PROJEN_GITHUB_TOKEN';
 
 const project = new AwsCdkConstructLibrary({
-  authorAddress: "mr.lin.clarence@gmail.com",
-  authorName: "Clarence Lin",
-  cdkVersion: "1.71.0",
-  name: "cdk-wordpress",
-  repository: "https://github.com/clarencetw/cdk-wordpress.git",
+  authorAddress: 'mr.lin.clarence@gmail.com',
+  authorName: 'Clarence Lin',
+  cdkVersion: '1.71.0',
+  name: 'cdk-wordpress',
+  repository: 'https://github.com/clarencetw/cdk-wordpress.git',
   keywords: ['aws', 'cdk', 'wordpress'],
   defaultReleaseBranch: 'master',
   depsUpgrade: DependenciesUpgradeMechanism.githubWorkflow({
@@ -29,12 +29,12 @@ const project = new AwsCdkConstructLibrary({
     module: 'cdk_wordpress',
   },
   cdkDependencies: [
-    "@aws-cdk/core",
-    "@aws-cdk/aws-ec2",
-    "@aws-cdk/aws-rds",
-    "@aws-cdk/aws-ecs",
-    "@aws-cdk/aws-ecs-patterns",
-    "@aws-cdk/aws-efs"
+    '@aws-cdk/core',
+    '@aws-cdk/aws-ec2',
+    '@aws-cdk/aws-rds',
+    '@aws-cdk/aws-ecs',
+    '@aws-cdk/aws-ecs-patterns',
+    '@aws-cdk/aws-efs',
   ],
 });
 
